6ddef090148e7a612026000427bd73dd924ff3d7
Victor Ndaba <mwangindaba@gmail.com>
2024-12-21 01:38:54 +0300

    feat: sharable secrets

--- infra/secrets.ts
+++ infra/secrets.ts
@@ -1,2 +1,16 @@
-export const contentfulSpaceId = new sst.Secret("ContentfulSpaceId");
-export const contentfulAccessToken = new sst.Secret("ContentfulAccessToken");
+export const contentfulSpaceId = secret("ContentfulSpaceId", { refId: "" });
+export const contentfulAccessToken = secret("ContentfulAccessToken", {
+  refId: "",
+});
+
+function secret(id: string, { refId }: { refId: string }) {
+  if ($app.stage === "dev") {
+    return new aws.ssm.Parameter(`SSM${id}`, {
+      type: "String",
+      value: new sst.Secret(id).value,
+    });
+  }
+
+  // reference existing secrets from dev stage
+  return aws.ssm.Parameter.get(id, refId);
+}
