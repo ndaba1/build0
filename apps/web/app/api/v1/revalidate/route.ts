09380402163b6c8c5a121a6a04a294fd2b15d628
Victor Ndaba <mwangindaba@gmail.com>
2024-12-21 04:54:31 +0300

    fix: use post method

--- apps/web/app/api/v1/revalidate/route.ts
+++ apps/web/app/api/v1/revalidate/route.ts
@@ -2,7 +2,7 @@ import { invalidateCloudFrontPaths } from "@/lib/cloudfront";
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest) => {
+export const POST = async (req: NextRequest) => {
   const secret = req.headers.get("x-revalidate-secret");
 
   if (!secret || secret !== process.env.REVALIDATE_SECRET) {
