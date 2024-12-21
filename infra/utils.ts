a2a1aca9ffa68d1b429197f73580a058f496bb95
Victor Ndaba <mwangindaba@gmail.com>
2024-12-21 23:26:00 +0300

    chore: remove cognito duplicate cookies

--- infra/utils.ts
+++ infra/utils.ts
@@ -50,7 +50,28 @@ async function handler(event) {
   }
   if(event.request.headers["cloudfront-viewer-longitude"]) {
     event.request.headers["x-open-next-longitude"] = event.request.headers["cloudfront-viewer-longitude"];
-  }      
+  }
+
+    const cookieNames = Object.keys(event.request.cookies);
+    const allowedCookies = {};
+
+    for (var i = cookieNames.length - 1; i >= 0; i--) {
+        const cookieName = cookieNames[i];
+
+        if (!cookieName.startsWith("CognitoIdentityServiceProvider")) {
+            allowedCookies[cookieName] = event.request.cookies[cookieName];
+        } else {
+            const parts = cookieName.split(".");
+            const suffix = parts[parts.length - 1];
+            const existingCookies = Object.keys(allowedCookies);
+
+            if (!existingCookies.some((cookie) => cookie.endsWith(suffix))) {
+                allowedCookies[cookieName] = event.request.cookies[cookieName];
+            }
+        }
+    }
+
+    event.request.cookies = allowedCookies;
     
     return event.request;
 }`;
