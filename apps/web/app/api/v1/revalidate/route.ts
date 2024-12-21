4c12b08ad1ce786055f5b0089d1aca8a712b67e6
Victor Ndaba <mwangindaba@gmail.com>
2024-12-21 03:37:24 +0300

    chore: store revalidation secret in headers

--- apps/web/app/api/v1/revalidate/route.ts
+++ apps/web/app/api/v1/revalidate/route.ts
@@ -2,9 +2,9 @@ import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-  const secret = req.nextUrl.searchParams.get("secret");
+  const secret = req.headers.get("x-revalidate-secret");
 
-  if (secret !== process.env.REVALIDATE_SECRET) {
+  if (!secret || secret !== process.env.REVALIDATE_SECRET) {
     return NextResponse.json({ error: "Invalid secret" }, { status: 401 });
   }
 
