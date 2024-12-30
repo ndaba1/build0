e494c305914c035c1e877222ebe1b266947d0f69
Victor Ndaba <mwangindaba@gmail.com>
2024-12-30 20:04:54 +0300

    chore: static page

--- apps/web/app/(public)/page.tsx
+++ apps/web/app/(public)/page.tsx
@@ -1,8 +1,5 @@
 import { Hero } from "./hero";
-import TheHow from "./the-how";
 import { TheWhat } from "./the-what";
-
-export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
