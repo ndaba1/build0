aa3f51de9c912fdc488700038995a8604365c042
Victor Ndaba <mwangindaba@gmail.com>
2025-01-02 19:42:17 +0300

    chore: add domain ui

--- apps/web/app/[slug]/(dashboard)/settings/domains/page-client.tsx
+++ apps/web/app/[slug]/(dashboard)/settings/domains/page-client.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
 import { useProject } from "@/hooks/use-project";
 
@@ -7,7 +9,7 @@ export function DomainsPageClient({ hasSubdomain }: { hasSubdomain: boolean }) {
   const { name } = useProject();
 
   return (
-    <main>
+    <main className="space-y-6">
       <div className="flex flex-row items-center justify-between rounded-lg border p-4 shadow-sm">
         <div className="space-y-0.5">
           <p className="font-medium">Project sub-domain</p>
@@ -17,7 +19,18 @@ export function DomainsPageClient({ hasSubdomain }: { hasSubdomain: boolean }) {
           </p>
         </div>
 
-        <Switch checked={hasSubdomain} />
+        <Switch checked={hasSubdomain} disabled={hasSubdomain} />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Input
+          className="h-12"
+          disabled={!hasSubdomain}
+          placeholder="Add a custom domain to your project. Enable project sub-domains before proceeding"
+        />
+        <Button size="lg" disabled={!hasSubdomain}>
+          Save
+        </Button>
       </div>
     </main>
   );
