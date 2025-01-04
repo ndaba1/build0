83bf9868a0d268c6a6429f37ba61387b9ceed101
Victor Ndaba <mwangindaba@gmail.com>
2025-01-04 22:30:34 +0300

    chore: configure cookie storage

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -6,10 +6,19 @@ import { authConfig } from "@/lib/auth";
 import { queryClient } from "@/lib/query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Amplify } from "aws-amplify";
+import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
+import { CookieStorage } from "aws-amplify/utils";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 Amplify.configure(authConfig, { ssr: true });
+
+cognitoUserPoolsTokenProvider.setKeyValueStorage(
+  new CookieStorage({
+    sameSite: "lax",
+    domain: ".buildzero.fyi",
+  })
+);
 
 if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
