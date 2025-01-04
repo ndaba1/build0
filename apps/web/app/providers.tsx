3027220111d404025f918ee6c26586ea80dd989c
Victor Ndaba <mwangindaba@gmail.com>
2025-01-04 22:22:15 +0300

    chore: patch amplify

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -6,20 +6,10 @@ import { authConfig } from "@/lib/auth";
 import { queryClient } from "@/lib/query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Amplify } from "aws-amplify";
-import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
-import { CookieStorage } from "aws-amplify/utils";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-cognitoUserPoolsTokenProvider.setKeyValueStorage(
-  new CookieStorage({
-    sameSite: "lax",
-    domain: ".buildzero.fyi",
-  })
-);
-
-// Don't need to set ssr to true since we're overriding the storage
-Amplify.configure(authConfig);
+Amplify.configure(authConfig, { ssr: true });
 
 if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
